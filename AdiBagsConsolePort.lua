6781d316ac71f68f765b280255915970c9cdb7f6
Marcin Ważeliński <mwazelinski@gmail.com>
2022-12-26 00:39:03 +0100

    adibags console port

---
+++ AdiBagsConsolePort.lua
@@ -0,0 +1,41 @@
+local addonName, addon = ...
+
+local AdiBags = LibStub("AceAddon-3.0"):GetAddon("AdiBags")
+
+local mod = AdiBags:NewModule('ConsolePortSupport', 'ABEvent-1.0')
+mod.uiName = 'Console Port support'
+mod.uiDesc = 'Highlight what changes in bags with a little sparkle.'
+
+local handlerButton = CreateFrame("Button", "AdiBags_ConsolePortSupport_BindingHandlerButton", nil)
+handlerButton:SetScript("OnClick", 
+    function(self, button, down)
+        local nmod = AdiBags:GetModule("NewItem")
+        local button = nmod.button
+        button:Click()
+    end
+)
+
+AdiBags:HookBagFrameCreation(mod, 
+    function(bag)
+        local frame = bag.frame
+        ConsolePort:AddInterfaceCursorFrame(frame)
+    end
+)
+
+EventRegistry:RegisterFrameEventAndCallback("PLAYER_ENTERING_WORLD", 
+    function()
+        mod:RegisterMessage("AdiBags_BagOpened", 
+            function(event, bagName, bag)
+                local frame = bag.frame
+                SetOverrideBindingClick(frame, true, "PAD3", handlerButton:GetName())
+            end
+        )
+
+        mod:RegisterMessage("AdiBags_BagClosed", 
+            function(event, bagName, bag)
+                local frame = bag.frame
+                ClearOverrideBindings(frame)
+            end
+        )
+    end, 
+self);
