0812babc3f3d9c10b525c088a8cb3924475dd8fe
Marcin Ważeliński <mwazelinski@gmail.com>
2022-12-29 01:42:04 +0100

    Fix some event handling

--- AdiBagsConsolePort.lua
+++ AdiBagsConsolePort.lua
@@ -6,36 +6,43 @@ local mod = AdiBags:NewModule('ConsolePortSupport', 'ABEvent-1.0')
 mod.uiName = 'Console Port support'
 mod.uiDesc = 'Highlight what changes in bags with a little sparkle.'
 
-local handlerButton = CreateFrame("Button", "AdiBags_ConsolePortSupport_BindingHandlerButton", nil)
-handlerButton:SetScript("OnClick", 
-    function(self, button, down)
-        local nmod = AdiBags:GetModule("NewItem")
-        local button = nmod.button
-        button:Click()
-    end
-)
+function RegisterBagEvents()
+    mod:RegisterMessage("AdiBags_BagOpened", 
+        function(event, bagName, bag)
+            local frame = bag.frame
+            SetOverrideBindingClick(frame, true, "PAD3", addon.handlerButton:GetName())
+        end
+    )
 
-AdiBags:HookBagFrameCreation(mod, 
-    function(bag)
-        local frame = bag.frame
-        ConsolePort:AddInterfaceCursorFrame(frame)
-    end
-)
+    mod:RegisterMessage("AdiBags_BagClosed", 
+        function(event, bagName, bag)
+            local frame = bag.frame
+            ClearOverrideBindings(frame)
+        end
+    )
+end
 
-EventRegistry:RegisterFrameEventAndCallback("PLAYER_ENTERING_WORLD", 
-    function()
-        mod:RegisterMessage("AdiBags_BagOpened", 
-            function(event, bagName, bag)
-                local frame = bag.frame
-                SetOverrideBindingClick(frame, true, "PAD3", handlerButton:GetName())
-            end
-        )
+function mod:OnEnable()
+    addon.handlerButton = CreateFrame("Button", "AdiBags_ConsolePortSupport_BindingHandlerButton", nil)
+    addon.handlerButton:SetScript("OnClick", 
+        function(self, button, down)
+            local nmod = AdiBags:GetModule("NewItem")
+            local button = nmod.button
+            button:Click()
+        end
+    )
 
-        mod:RegisterMessage("AdiBags_BagClosed", 
-            function(event, bagName, bag)
-                local frame = bag.frame
-                ClearOverrideBindings(frame)
-            end
-        )
-    end, 
-self);
+    AdiBags:HookBagFrameCreation(mod, 
+        function(bag)
+            local frame = bag.frame
+            ConsolePort:AddInterfaceCursorFrame(frame)
+        end
+    )
+
+    EventRegistry:RegisterFrameEventAndCallback("PLAYER_ENTERING_WORLD", RegisterBagEvents, self)
+    RegisterBagEvents()
+end
+
+function mod:OnDisable()
+    print("Reload UI to disable AdiBags Console Port support")
+end
